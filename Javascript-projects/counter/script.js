972f52d863b9b492ba90e526aebc42933579d39f
HIDRI A <agthidri24@yahoo.com>
2022-11-07 23:49:13 -0600

    this commit will add lab5

--- Javascript-projects/counter/script.js
+++ Javascript-projects/counter/script.js
@@ -1,3 +1,31 @@
+/** 
+ * app2.js
+ */
+var p = 50;
+var q = 100;
+var r = function (p, q, r) {
+    console.log(`a = ${a}`);
+    console.log("q =" + q);
+    var s = function (p, q, r) {
+        p = r;
+        console.log(`p=${p}`);
+        p = q;
+        r = r * 2;
+        var a = 75;
+        return r;
+    }
+    return s(p, q, r);
+}
+a = 17;
+q = r(18, 19, 100);
+console.log("p= " + p);
+console.log(`q = ${q}`);
+
+
+
+
+
+
 var count = 0;
 
 function changeCount(num) {
@@ -5,3 +33,14 @@ function changeCount(num) {
     document.getElementById("count").innerHTML =
         count;
 }
+
+function codeHoist() {
+    a = 10;
+    let b = 50;
+    b = 50;
+}
+codeHoist();
+
+console.log(a); // 10
+console.log(b);
+
